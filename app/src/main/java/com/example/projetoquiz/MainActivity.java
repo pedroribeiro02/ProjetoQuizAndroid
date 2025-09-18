8c9e5d2b881aafcb84bc92a3c04b76ffc68d965a
pedroribeiro02 <pedrodossantosribeiro123@gmail.com>
2025-09-18 09:18:58 -0300

    Vetores criados

--- app/src/main/java/com/example/projetoquiz/MainActivity.java
+++ app/src/main/java/com/example/projetoquiz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projetoquiz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    TextView txvPergunta;
+    RadioGroup rdgPrincipal;
+    Button btnResponder;
+    int pontuacao = 0;
+    int indicePergunta = 0;
+
+
+    String[] perguntas = {
+            "Minha pergunta 1",
+            "Minha pergunta 2",
+            "Minha pergunta 3",
+            "Minha pergunta 4",
+            "Minha pergunta 5"
+    };
+    int []respostasCorretas = {
+            R.id.rb_a,
+            R.id.rb_a,
+            R.id.rb_b,
+            R.id.rb_d,
+            R.id.rb_c
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
